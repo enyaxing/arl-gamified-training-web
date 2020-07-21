c3d6234131599df2cc3f6ec60ade12fad6407981
DragonKyro <kyle31412@gmail.com>
2020-07-21 11:44:43 -0400

    nav bar

--- src/studentHome.js
+++ src/studentHome.js
@@ -1,26 +1,60 @@
 import React, { Component } from "react";
 import firebase from "./config/Fire";
 import Upload from "./components/upload";
+import NavBar from "./components/navbar";
+import Home from "./studentPages/home";
+import Question from "./studentPages/question";
+import Training from "./studentPages/training";
+import Gonogo from "./studentPages/gonogo";
+import Settings from "./studentPages/settings";
+import Profile from "./studentPages/profile";
+import About from "./studentPages/about";
 
 class StudentHome extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+        location : "home"
     };
+    this.setLocation = this.setLocation.bind(this)
   }
 
   logout = () => {
     firebase.auth().signOut();
   };
 
+  setLocation = (loc) => {
+      this.setState({ location : loc })
+      console.log(loc)
+  };
+
+  changeLocation = () => {
+    switch(this.state.location) {
+        case 'question':
+            return (<Question />);
+        case 'training':
+            return (<Training />);
+        case 'gonogo':
+            return (<Gonogo />);
+        case 'settings':
+            return (<Settings />);
+        case 'profile':
+            return (<Profile />);
+        case 'about':
+            return (<About />);
+        default:
+            return (<Home />);
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Student Home</h1>
+        <h1>Student Portal</h1>
         <p>{this.props.user.email}</p>
+        <NavBar setLocation = {this.setLocation} />
+        {this.changeLocation()}
         <button onClick={this.logout}>Sign out</button>
-        <Upload />
       </div>
     );
   }
---
+++ src/studentPages/training.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Training extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h1>training</h1>
+    );
+  }
+}
+
+export default Training;
